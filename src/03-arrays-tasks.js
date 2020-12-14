ad6e21072da74bb6f578260fee947f6dd0985ef5
Никита Гавшин <git add>
2020-12-14 22:12:53 +0400

    feat:add array

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -118,8 +118,9 @@ function removeFalsyValues(/* arr */) {
  *    => [ 'PERMANENT-INTERNSHIP', 'GLUTINOUS-SHRIEK', 'MULTIPLICATIVE-ELEVATION' ],
  *    [ 'a', 'b', 'c', 'd', 'e', 'f', 'g' ]  => [ 'A', 'B', 'C', 'D', 'E', 'F', 'G' ]
  */
-function getUpperCaseStrings(/* arr */) {
-  throw new Error('Not implemented');
+function getUpperCaseStrings(arr) {
+  const arrFil = arr.map((num) => num.toUpperCase());
+  return arrFil;
 }
 
 
@@ -133,8 +134,9 @@ function getUpperCaseStrings(/* arr */) {
  *    [ '', 'a', 'bc', 'def', 'ghij' ]  => [ 0, 1, 2, 3, 4 ]
  *    [ 'angular', 'react', 'ember' ] => [ 7, 5, 5 ]
  */
-function getStringsLength(/* arr */) {
-  throw new Error('Not implemented');
+function getStringsLength(arr) {
+  const arrFil = arr.map((num) => num.length);
+  return arrFil;
 }
 
 /**
